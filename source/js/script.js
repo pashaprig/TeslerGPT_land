e18770c82097c3d12206c35971bb9ee580e5c813
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-12-06 17:04:02 +0200

    Mobile nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,31 @@
-console.log('JS')
+// MENU
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var navButtonText = document.querySelector('.main-nav__open-btn-text');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navButtonText.classList.add('visually-hidden');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navButtonText.classList.remove('visually-hidden');
+  }
+});
+
+const links = document.querySelectorAll('.main-nav__item');
+
+const navLinckHandleClick = () => {
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+  navButtonText.classList.remove('visually-hidden');
+}
+
+links.forEach(link => {
+  link.addEventListener('click', navLinckHandleClick)
+})
