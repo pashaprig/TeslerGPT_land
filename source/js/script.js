272ffd9fdeccf60365e3d6c66ad5195af716b59b
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2023-05-02 13:22:01 +0300

    Class organization of JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,31 +1,61 @@
 // MENU
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var navButtonText = document.querySelector('.main-nav__open-btn-text');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navButtonText.classList.add('visually-hidden');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navButtonText.classList.remove('visually-hidden');
+class App {
+  init() {
+    this.initMobileMenu();
+    this.initLog();
   }
-});
 
-const links = document.querySelectorAll('.main-nav__item');
+  initMobileMenu() {
+    const navMain = document.querySelector('.main-nav');
+    const navToggle = document.querySelector('.main-nav__toggle');
+    const navButtonText = document.querySelector('.main-nav__open-btn-text');
 
-const navLinckHandleClick = () => {
-  navMain.classList.add('main-nav--closed');
-  navMain.classList.remove('main-nav--opened');
-  navButtonText.classList.remove('visually-hidden');
+    const initJS = () => {
+      navMain.classList.remove('main-nav--nojs');
+    }
+
+    const closeOpen = () => {
+      navToggle.addEventListener('click', function () {
+        if (navMain.classList.contains('main-nav--closed')) {
+          navMain.classList.remove('main-nav--closed');
+          navMain.classList.add('main-nav--opened');
+          navButtonText.classList.add('visually-hidden');
+        } else {
+          navMain.classList.add('main-nav--closed');
+          navMain.classList.remove('main-nav--opened');
+          navButtonText.classList.remove('visually-hidden');
+        }
+      });
+    }
+
+    const linksClick = () => {
+      const links = document.querySelectorAll('.main-nav__item');
+
+      const navLinckHandleClick = () => {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+        navButtonText.classList.remove('visually-hidden');
+      }
+
+      links.forEach(link => {
+        link.addEventListener('click', navLinckHandleClick)
+      })
+    }
+
+    initJS();
+    closeOpen();
+    linksClick();
+  }
+
+  initLog() {
+    const log = () => {
+      console.log('log')
+    }
+
+    log();
+  }
 }
 
-links.forEach(link => {
-  link.addEventListener('click', navLinckHandleClick)
-})
+const app = new App();
+document.addEventListener('DOMContentLoaded', app.init());
